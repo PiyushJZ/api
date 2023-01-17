140be6c6ab967eff2011862cfdbc8582b3811ccc
Piyush Jain <piyushjz0402@gmail.com>
2023-01-17 15:28:54 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD57y6b5RTE9qMS0OvszayTahGqhOBPzgk",
+  authDomain: "streamy-32bf8.firebaseapp.com",
+  projectId: "streamy-32bf8",
+  storageBucket: "streamy-32bf8.appspot.com",
+  messagingSenderId: "178299201761",
+  appId: "1:178299201761:web:a0d9535296dd3a22d9afa0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
